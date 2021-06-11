b3e3948d292e0228a48c358fff743854f3b577ea
Daniel Danis <daniel.danis@jax.org>
2021-06-11 13:46:37 -0400

    Add modules

---
+++ fenominal-cli/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module fenominal.cli {
+    requires fenominal.core;
+
+    requires phenol.core;
+    requires phenol.io;
+    requires info.picocli;
+    requires org.apache.commons.net;
+
+    requires org.slf4j;
+}
