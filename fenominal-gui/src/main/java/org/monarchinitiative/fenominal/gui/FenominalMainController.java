ddbaf3e14544527ffd883263b78bc37c9c4289fa
pnrobinson <peter.robinson@jax.org>
2021-06-17 22:00:45 -0400

    adding table view

--- fenominal-gui/src/main/java/org/monarchinitiative/fenominal/gui/FenominalMainController.java
+++ fenominal-gui/src/main/java/org/monarchinitiative/fenominal/gui/FenominalMainController.java
@@ -1,15 +1,23 @@
 package org.monarchinitiative.fenominal.gui;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.Clipboard;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import org.controlsfx.control.spreadsheet.GridBase;
+import org.controlsfx.control.spreadsheet.SpreadsheetCell;
+import org.controlsfx.control.spreadsheet.SpreadsheetCellType;
+import org.controlsfx.control.spreadsheet.SpreadsheetView;
+import org.controlsfx.dialog.CommandLinksDialog;
 import org.monarchinitiative.fenominal.core.FenominalRunTimeException;
 import org.monarchinitiative.fenominal.gui.guitools.PopUps;
 import org.monarchinitiative.fenominal.gui.io.HpoMenuDownloader;
@@ -22,6 +30,7 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.stream.Collectors;
@@ -30,15 +39,16 @@ import java.util.stream.Collectors;
 public class FenominalMainController {
 
     @FXML
-    public TextArea parseArea;
-    @FXML
     public Button parseButton;
-    @FXML
-    public Button pasteClipboard;
+
     @FXML public Button importTextFile;
 
     @FXML
     public Label hpoReadyLabel;
+
+    @FXML
+    public SpreadsheetView spreadSheetView;
+
     private FenominalMiner fenominalMiner = null;
     private final ExecutorService executor;
 
@@ -68,10 +78,21 @@ public class FenominalMainController {
         this.executor.submit(task);
         // only enable analyze if Ontology downloaded (enabled property watches
         parseButton.disableProperty().bind(optionalResources.ontologyProperty().isNull());
-//        optionalResources.ontologyProperty().addListener((observable,oldValue,newValue) -> { if (newValue!=null) {
-//            this.hpoReadyLabel.setText("");
-//        }
-//       });
+        // set up table view with
+        int rowCount = 6;
+        int columnCount = 2;
+        GridBase grid = new GridBase(rowCount, columnCount);
+        ObservableList<ObservableList<SpreadsheetCell>> rows = FXCollections.observableArrayList();
+        for (int row = 0; row < grid.getRowCount(); ++row) {
+            final ObservableList<SpreadsheetCell> list = FXCollections.observableArrayList();
+            for (int column = 0; column < grid.getColumnCount(); ++column) {
+                list.add(SpreadsheetCellType.STRING.createCell(row, column, 1, 1,"value"));
+            }
+            rows.add(list);
+        }
+        grid.setRows(rows);
+        this.spreadSheetView.setGrid(grid);
+
     }
 
 
@@ -96,7 +117,7 @@ public class FenominalMainController {
                     .withPhenotypeTerms(new HashSet<>()) // maybe you want to display some terms from the beginning
                     .build();
             // get reference to primary stage
-            Window w = this.parseArea.getScene().getWindow();
+            Window w = this.parseButton.getScene().getWindow();
 
             // show the text mining analysis dialog in the new stage/window
             Stage secondary = new Stage();
@@ -117,17 +138,43 @@ public class FenominalMainController {
         e.consume();
     }
     @FXML
-    private void pasteClipboard(ActionEvent e) {
-        Clipboard clipboard = Clipboard.getSystemClipboard();
-        if (clipboard.hasString()) {
-            this.parseArea.setText(clipboard.getString());
+    private void getStarted(ActionEvent e) {
+        var caseReport = new CommandLinksDialog.CommandLinksButtonType("Case report","Enter data about one individual, one time point", true);
+        var caseReportTemporal = new CommandLinksDialog.CommandLinksButtonType("Case report (multiple time points)","Enter data about one individual, multiple time points", false);
+        var cohortTogether = new CommandLinksDialog.CommandLinksButtonType("Cohort","Enter data about cohort", false);
+        var cohortOneByOne = new CommandLinksDialog.CommandLinksButtonType("Cohort (enter data about multiple individuals)","Enter data about cohort (one by one)", false);
+        CommandLinksDialog dialog = new CommandLinksDialog(caseReport, caseReportTemporal, cohortTogether, cohortOneByOne);
+        dialog.setTitle("Get started");
+        dialog.setHeaderText("Select type of curation");
+        dialog.setContentText("Fenominal supports four types of HPO biocuration");
+        Optional<ButtonType> opt = dialog.showAndWait();
+        if (opt.isPresent()) {
+            ButtonType btype = opt.get();
+            switch(btype.getText()) {
+                case "Case report":
+                    System.out.println("case report");
+                    break;
+                case "Case report (multiple time points)":
+                    System.out.println("case report temporal");
+                    break;
+                case "Cohort":
+                    System.out.println("cohortTogether");
+                    break;
+                case "Cohort (enter data about multiple individuals)":
+                    System.out.println("cohortOneByOne");
+                    break;
+                default:
+                    System.out.println("What button??");
+            }
+
         }
+
         e.consume();
     }
 
     @FXML
     private void importHpJson(ActionEvent e) {
-        //this.optionalResources.
+
         String fname = this.appHomeDirectory.getAbsolutePath() + File.separator + OptionalResources.DEFAULT_HPO_FILE_NAME;
         HpoMenuDownloader downloader = new HpoMenuDownloader();
         try {
