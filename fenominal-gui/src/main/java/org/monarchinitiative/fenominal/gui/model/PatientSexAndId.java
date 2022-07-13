94b011b9658bde35f7f00d0660914d06d03fd98e
Peter Robinson <peter.robinson@jax.org>
2022-07-13 18:15:08 -0400

    moving GUI to separate repository

--- fenominal-gui/src/main/java/org/monarchinitiative/fenominal/gui/model/PatientSexAndId.java
+++
@@ -1,7 +0,0 @@
-package org.monarchinitiative.fenominal.gui.model;
-
-/**
- * POJO for returning results about patient sex and the anonymized ID
- */
-public record PatientSexAndId(Sex sex, String id) {
-}
--- fenominal-gui/src/main/java/org/monarchinitiative/fenominal/gui/model/PatientSexIdAndBirthdate.java
+++
@@ -1,9 +0,0 @@
-package org.monarchinitiative.fenominal.gui.model;
-
-import java.time.LocalDate;
-
-/**
- * POJO for returning results about patient sex and the anonymized ID and the birthdate
- */
-public record PatientSexIdAndBirthdate(Sex sex, String id, LocalDate birthdate) {
-}
