82ca27df6838b019c967eb70fe180d92cb826903
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-04-10 17:27:16 -0400

    Remove unused `core.impl.json` package and Jackson libraries.

--- fenominal-core/src/main/java/module-info.java
+++ fenominal-core/src/main/java/module-info.java
@@ -2,8 +2,6 @@ module org.monarchinitiative.fenominal.core {
     // Ontology is in the TermMiner API (static methods).
     requires transitive org.monarchinitiative.phenol.core;
 
-    requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.databind;
     requires org.slf4j;
 
     exports org.monarchinitiative.fenominal.model;
