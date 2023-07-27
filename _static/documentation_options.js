e4bc84c8f82ed531901da450ccc639162a737510
ielis <daniel.gordon.danis@protonmail.com>
2023-07-27 21:12:11 +0000

    Deploying to gh-pages from @ monarch-initiative/fenominal@41fd10d947a649a53200caaabedacf3a039d965a 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.2-SNAPSHOT',
+    VERSION: 'v0.7.10',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
