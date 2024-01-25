760b227e5b11a16c1ea298434e9c7383f4bb1687
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-01-25 16:47:20 -0500

    Update Maven plugins and dependencies. Remove some redundant tests.

--- fenominal-core/src/test/java/org/monarchinitiative/fenominal/core/TestResources.java
+++ fenominal-core/src/test/java/org/monarchinitiative/fenominal/core/TestResources.java
@@ -9,9 +9,7 @@ public class TestResources {
 
     public static final Path TEST_BASE = Path.of("src/test/resources");
     private static final Path HPO_PATH = TEST_BASE.resolve("hpo").resolve("hp.json");
-    private static final Path SMALL_HPO_PATH = TEST_BASE.resolve("hpo").resolve("hp_head.json");
     private static volatile MinimalOntology HPO;
-    private static volatile MinimalOntology SMALL_HPO;
 
     public static MinimalOntology hpo() {
         if (HPO == null) {
@@ -21,16 +19,6 @@ public class TestResources {
             }
         }
         return HPO;
-    }
-
-    public static MinimalOntology smallHpo() {
-        if (SMALL_HPO == null) {
-            synchronized (TestBase.class) {
-                if (SMALL_HPO == null)
-                    SMALL_HPO = MinimalOntologyLoader.loadOntology(SMALL_HPO_PATH.toFile());
-            }
-        }
-        return SMALL_HPO;
     }
 
     private TestResources(){}
